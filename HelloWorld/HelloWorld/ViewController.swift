271a91c4ac8cd59b5fc537fae303630d97a3ff9b
AppleDevApp <appledevapp@AppleDevApp.local>
2017-10-10 23:00:16 +0800

    1.Initial the project

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by AppleDevApp on 10/10/17.
+//  Copyright © 2017年 AppleDevApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
